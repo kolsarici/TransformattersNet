779a8d39e3dc93ae6d360a25961f67f9530b5ef2
kolsarici <kolsarici@hotmail.com>
2018-04-03 22:51:26 +0300

    validateInput false JsonController içinde yapıldı

--- Kolsarici.Transformatters.MvcWebUI/Controllers/JsonController.cs
+++ Kolsarici.Transformatters.MvcWebUI/Controllers/JsonController.cs
@@ -10,6 +10,7 @@ using System.Web.Script.Serialization;
 
 namespace Kolsarici.Transformatters.MvcWebUI.Controllers
 {
+    [ValidateInput(false)]
     public class JsonController : Controller
     {
         private IJsonService _jsonService;
